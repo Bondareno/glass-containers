35bee6fd351061ee2aa9e36f099d284a4b374123
Bondareno <146577910+Bondareno@users.noreply.github.com>
2023-10-28 07:22:05 +0300

    Update common_minion.py

--- filesmeta/common_minion.py
+++ filesmeta/common_minion.py
@@ -1,1 +1,8 @@
-
+class CommonMinion:
+    def get_meta_info(self, path):
+        metadata = {
+            "size": os.path.getsize(path),
+            "created_at": os.path.getctime(path),
+            "modified_at": os.path.getmtime(path),
+        }
+        return metadata
