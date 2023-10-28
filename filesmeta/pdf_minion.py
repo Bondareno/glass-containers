6b1f6b652e60c2ebc6668d9f74f74ad0ef2456d6
Bondareno <146577910+Bondareno@users.noreply.github.com>
2023-10-28 07:23:35 +0300

    Update pdf_minion.py

--- filesmeta/pdf_minion.py
+++ filesmeta/pdf_minion.py
@@ -1,1 +1,32 @@
+from PyPDF2 import PdfFileReader
+import os
+
+
+class PDFMinion:
+    EX = {"pdf"}
+
+    def get_meta_info(self, path):
+        metadata = {}
+        try:
+            if path.split(".")[-1].lower() in self.EX:
+                with open(path, 'rb') as pdf_file:
+                    pdf_reader = PdfFileReader(pdf_file)
+                    num_pages = pdf_reader.getNumPages()
+                    author = pdf_reader.getDocumentInfo().author
+                    creator = pdf_reader.getDocumentInfo().creator
+                    producer = pdf_reader.getDocumentInfo().producer
+                    subject = pdf_reader.getDocumentInfo().subject
+                    title = pdf_reader.getDocumentInfo().title
+
+                metadata = {
+                    "Number of Pages": num_pages,
+                    "Author": author,
+                    "Creator": creator,
+                    "Producer": producer,
+                    "Subject": subject,
+                    "Title": title,
+                }
+            return metadata
+        except Exception as e:
+            return {"error": str(e)}
 
