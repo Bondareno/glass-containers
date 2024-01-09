e6d6213ac18403232f3bd0c11914ff4cf5a83a30
Ekaterina Bondarenko <146577910+Bondareno@users.noreply.github.com>
2024-01-09 16:13:48 +0300

    Update top_10_files_weight.py

--- utils/top_10_files_weight.py
+++ utils/top_10_files_weight.py
@@ -1,23 +1,27 @@
 import os
 import time
+import sqlite3
 
-def get_10_largest_files(root_path):
+def get_10_largest_files(db_path):
     start_time = time.time()
     file_list = []
 
-    for root, _, files in os.walk(root_path):
-        for filename in files:
-            file_path = os.path.join(root, filename)
-            try:
-                file_size = os.path.getsize(file_path)
-                file_list.append((file_path, file_size))
-            except (FileNotFoundError, PermissionError):
-                pass
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
 
-    ten_largest_files = sorted(file_list, key=lambda x: x[1], reverse=True)[:10]
+    cursor.execute("""
+        SELECT path, size
+        FROM index_files
+        ORDER BY size DESC
+        LIMIT 10
+    """)
+
+    ten_largest_files = cursor.fetchall()
 
     for i, (file_path, file_size) in enumerate(ten_largest_files, start=1):
         print(f"{i}) File: {file_path}, Size: {file_size} bytes")
+
+    connection.close()
 
     end_time = time.time()
     elapsed_time = end_time - start_time
@@ -25,6 +29,6 @@ def get_10_largest_files(root_path):
     return elapsed_time
 
 if __name__ == "__main__":
-    root_path = os.path.abspath(os.sep)
-    elapsed_time = get_10_largest_files(root_path)
+    db_path = "data/index.sqlite"
+    elapsed_time = get_10_largest_files(db_path)
     print(f"Program execution time: {elapsed_time:.2f} seconds")
