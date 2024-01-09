32b77a0eda07190654d7c4e811a76c5865cd3d73
Ekaterina Bondarenko <146577910+Bondareno@users.noreply.github.com>
2024-01-09 16:19:50 +0300

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template
-from utils import count_files, top_10_ext, top_10_files_weight
-from filesmeta import common_minion, doc_minion, gru, img_minion, pdf_minion
+from utils.files_count import files_count
+from utils.extensions_count import extensions_count
+from utils.top_by_size import top_by_size
 
 app = Flask(__name__)
 
+db_path = 'data/index.sqlite'
+
 @app.route('/')
 def index():
-    num_of_files = count_files.get_num_of_files()
-    top_extensions = top_10_ext.get_top_10_extensions()
-    top_files_weight = top_10_files_weight.get_top_10_files_weight()
+    mod_time = f"Last Modified: {time.strftime('%Y-%m-%d', time.localtime(os.path.getmtime(db_path)))}"
+    files_count = f_count(db_path)
+    return render_template('index.html', mod_time=mod_time, files_count=files_count)
 
-    return render_template('index.html', num_of_files=num_of_files, top_extensions=top_extensions, top_files_weight=top_files_weight)
+@app.route('/ext_stat')
+def ext_stat():
+    res = extensions_count(db_path, 10)
+    return render_template('ext_stat.html', extensions=res)
+
+@app.route('/top_10_by_size')
+def top_10_by_size():
+    res = top_by_size(db_path, 10)
+    return render_template('top_10_by_size.html', top_files=res)
 
 if __name__ == '__main__':
     app.run(debug=True)
